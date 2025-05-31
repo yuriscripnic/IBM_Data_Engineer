42a54d797c06c7e7b9d740445a78864dbf4e1055
Yuri Scripnic <yuriscripnic@gmail.com>
2025-05-31 23:44:56 +0900

    Solve practice problem

---
+++ Python_Project_Data_Engineering/etl_project_gdp.py
@@ -0,0 +1,160 @@
+"""
+URL:'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
+
+table_attribs: The attributes or column names for the dataframe stored as a list. Since the data available in the website is in USD Millions, the attributes should initially be 'Country' and 'GDP_USD_millions'.This will be modified in the transform function later.
+db_name: As mentioned in the Project scenario, 'World_Economies.db'
+table_name: As mentioned in the Project scenario, 'Countries_by_GDP'
+csv_path: As mentioned in the Project scenario, 'Countries_by_GDP.csv'
+
+
+log_progress('Preliminaries complete. Initiating ETL process')
+
+df = extract(url, table_attribs)
+
+log_progress('Data extraction complete. Initiating Transformation process')
+
+df = transform(df)
+
+log_progress('Data transformation complete. Initiating loading process')
+
+load_to_csv(df, csv_path)
+
+log_progress('Data saved to CSV file')
+
+sql_connection = sqlite3.connect('World_Economies.db')
+
+log_progress('SQL Connection initiated.')
+
+load_to_db(df, sql_connection, table_name)
+
+log_progress('Data loaded to Database as table. Running the query')
+
+query_statement = f"SELECT * from {table_name} WHERE GDP_USD_billions >= 100"
+run_query(query_statement, sql_connection)
+
+log_progress('Process Complete.')
+
+sql_connection.close()
+
+"""
+
+
+# Importing the required libraries
+
+import sqlite3
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup
+import requests
+from datetime import datetime 
+
+# Code for ETL operations on Country-GDP data
+
+timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
+url = 'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
+table_attribs = ['Country','GDP_USD_millions']
+db_name = 'World_Economies.db'
+table_name = 'Countries_by_GDP'
+csv_path = 'Countries_by_GDP.csv'
+query_statement = f"SELECT * from {table_name} WHERE GDP_USD_billions >= 100"
+
+def extract(url: str, table_attribs):
+    ''' This function extracts the required
+    information from the website and saves it to a dataframe. The
+    function returns the dataframe for further processing. '''
+    
+    page_data =  requests.get(url).text
+    
+    parser = BeautifulSoup(page_data, features='html.parser')
+    
+    df = pd.DataFrame(columns= table_attribs)
+    
+    page_tables = parser.find_all('tbody')
+    
+    table_rows = page_tables[2].find_all('tr')
+    
+    for row in table_rows:
+        col = row.find_all('td')
+        if len(col)!=0:
+            if col[0].find('a') is not None and '—' not in col[2]:
+                data_dict = {table_attribs[0]: col[0].a.contents[0],
+                             table_attribs[1]: col[2].contents[0]}
+                df1 = pd.DataFrame(data_dict, index=[0])
+                df = pd.concat([df,df1], ignore_index=True)
+    
+    log_progress(df.head())
+    return df
+        
+def transform(df: pd.DataFrame):
+    ''' This function converts the GDP information from Currency
+    format to float value, transforms the information of GDP from
+    USD (Millions) to USD (Billions) rounding to 2 decimal places.
+    The function returns the transformed dataframe.'''
+    gdp_list = df['GDP_USD_millions'].to_list()
+    gdp_list = [float(''.join(x.split(','))) for x in gdp_list]
+    gdp_list = [np.round(x/1000) for x in gdp_list] 
+    df['GDP_USD_millions'] = gdp_list
+    df = df.rename(columns={'GDP_USD_millions':'GDP_USD_billions'})
+
+    log_progress(df.head())
+    return df
+
+def load_to_csv(df: pd.DataFrame, csv_path: str):
+    ''' This function saves the final dataframe as a `CSV` file 
+    in the provided path. Function returns nothing.'''
+    df.to_csv(csv_path)
+
+def load_to_db(df: pd.DataFrame, sql_connection: sqlite3.Connection, table_name):
+    ''' This function saves the final dataframe as a database table
+    with the provided name. Function returns nothing.'''
+    df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
+
+def run_query(query_statement, sql_connection: sqlite3.Connection):
+    ''' This function runs the stated query on the database table and
+    prints the output on the terminal. Function returns nothing. '''
+    log_progress(f"SQL Query executed: {query_statement}")
+    log_progress("Results:")
+    log_progress(pd.read_sql_query(query_statement, sql_connection).head())
+
+def log_progress(message):
+    ''' This function logs the mentioned message at a given stage of the code execution to a log file. Function returns nothing'''
+    
+    now = datetime.now() # get current timestamp 
+    timestamp = now.strftime(timestamp_format) 
+    with open("./etl_project_log.txt","a") as f: 
+        f.write(f"{timestamp} : {message}\n")
+    
+    
+''' Here, you define the required entities and call the relevant 
+functions in the correct order to complete the project. Note that this
+portion is not inside any function.'''
+
+if __name__ == "__main__":
+
+    with sqlite3.connect('World_Economies.db') as sql_connection:
+        log_progress('Preliminaries complete. Initiating ETL process')
+
+        df = extract(url, table_attribs)
+
+        log_progress(f'Data extraction complety from {url}.\nInitiating Transformation process')
+
+        df = transform(df)
+
+        log_progress('Data transformation complete. Initiating loading process')
+
+        load_to_csv(df, csv_path)
+
+        log_progress(f'Data saved to CSV file: : {csv_path}')
+
+
+
+        log_progress('SQL Connection initiated.')
+
+        load_to_db(df, sql_connection, table_name)
+
+        log_progress('Data loaded to Database as table. Running the query')
+
+        query_statement = f"SELECT * from {table_name} WHERE GDP_USD_billions >= 100"
+        run_query(query_statement, sql_connection)
+
+        log_progress('Process Complete.')
